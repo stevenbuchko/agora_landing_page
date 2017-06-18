c4debef7a5b383ad9ff2f2b51d9068e34147ddff
Steven <stevenbuchko@gmail.com>
2017-06-18 14:50:05 -0400

    added success banner to product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-$('#product-contact-form').submit(function (e) {
-  $('email-success-alert').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
+jQuery('#product-contact-form').submit(function (e) {
+  jQuery('email-success-alert').removeClass('hide').addClass('alert alert-success alert-dismissible').slideDown().show();
   e.preventDefault();
   return false;
 });
